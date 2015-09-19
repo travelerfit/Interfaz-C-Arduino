5552d037271329a2723f73d61e4449bc60530881
David <davidsosajorge@gmail.com>
2015-09-18 23:50:36 -0500

    interfaz

---
+++ Interfaz C#/EncenderLED/EncenderLED/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frmTurnONTurnOFFLED
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.btnTurnON = new System.Windows.Forms.Button();
+            this.btnClosePort = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // serialPort
+            // 
+            this.serialPort.PortName = "COM3";
+            // 
+            // btnTurnON
+            // 
+            this.btnTurnON.Location = new System.Drawing.Point(71, 106);
+            this.btnTurnON.Name = "btnTurnON";
+            this.btnTurnON.Size = new System.Drawing.Size(167, 40);
+            this.btnTurnON.TabIndex = 0;
+            this.btnTurnON.Text = "Enviar";
+            this.btnTurnON.UseVisualStyleBackColor = true;
+            this.btnTurnON.Click += new System.EventHandler(this.btnTurnON_Click);
+            // 
+            // btnClosePort
+            // 
+            this.btnClosePort.Location = new System.Drawing.Point(169, 281);
+            this.btnClosePort.Name = "btnClosePort";
+            this.btnClosePort.Size = new System.Drawing.Size(167, 40);
+            this.btnClosePort.TabIndex = 2;
+            this.btnClosePort.Text = "Cerrar puerto serial";
+            this.btnClosePort.UseVisualStyleBackColor = true;
+            this.btnClosePort.Click += new System.EventHandler(this.btnClosePort_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(301, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Introduce un numero del 1 al 4 ";
+            // 
+            // frmTurnONTurnOFFLED
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 350);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClosePort);
+            this.Controls.Add(this.btnTurnON);
+            this.Name = "frmTurnONTurnOFFLED";
+            this.Text = "Electrovalvula";
+            this.Load += new System.EventHandler(this.frmTurnONTurnOFFLED_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Button btnTurnON;
+        private System.Windows.Forms.Button btnClosePort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Interfaz C#/EncenderLED/EncenderLED/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frmTurnONTurnOFFLED : Form
+    {
+        public frmTurnONTurnOFFLED()
+        {
+            InitializeComponent();
+        }
+        private void btnTurnON_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string d = textBox1.Text;
+
+                //serialPort1.Write("1"); //send 1 to Arduino
+                byte[] mBuffer = Encoding.ASCII.GetBytes(d);
+                serialPort.Write(mBuffer, 0, mBuffer.Length);
+
+
+                int intReturnASCII = 0;
+                int count = serialPort.BytesToRead;
+                string returnMessage = "";
+                while (count > 0)
+                {
+                    intReturnASCII = serialPort.ReadByte();
+                    returnMessage = returnMessage + Convert.ToChar(intReturnASCII);
+                    count--;
+                }
+
+                               
+                string result = returnMessage;
+
+                //serialPort.Read(mBuffer, 0, mBuffer.Length);
+
+                
+
+                label1.Text= result;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void btnTurnOFF_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //serialPort1.Write("0"); //send 0 to Arduino
+                byte[] mBuffer = Encoding.ASCII.GetBytes("apagar");
+                serialPort.Write(mBuffer, 0, mBuffer.Length);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void frmTurnONTurnOFFLED_Load(object sender, EventArgs e)
+        {
+            serialPort.Open(); //open serialPort
+        }
+
+        private void btnClosePort_Click(object sender, EventArgs e)
+        {
+            serialPort.Close(); //close serialPort
+        }
+
+        private void btnTurnON_Click_1(object sender, EventArgs e)
+        {
+
+        }
+       
+     
+    }
+}
